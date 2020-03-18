4030fbf954a3c0672363a464afbe3200d2b0bc25
TangRufus <tangrufus@gmail.com>
2020-03-18 13:42:08 +0000

    :mask::mask::mask: Leave me alone

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+header('X-Robots-Tag: noindex, nofollow', true, 410);
+?>
+<!doctype html>
+<html lang="en-GB">
+<head>
+  <meta charset="utf-8">
+  <title>Site has been removed</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="noindex, nofollow">
+</head>
+
+<body>
+  <h1>Site has been removed</h1>
+  <p>If you need to access the staging sites, please contact the support team at <a href="mailto:support@itineris.co.uk">support@itineris.co.uk</a>.</p>
+  <p>©<?php echo date('Y'); ?> <a href="https://www.itineris.co.uk/">Itineris Limited</a> - All rights reserved.</p>
+</body>
+</html>
